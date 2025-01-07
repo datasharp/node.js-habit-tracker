2d2fef52dee3d8049b93a8d063e079c7bc50d642
datasharp <krs.sharp00@gmail.com>
2025-01-06 16:52:01 -0800

    updated backend and frontend folder names

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,19 @@ import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // Import Components, styles, media
-import Navigation from './components/Navigation';
+import Navigation from './components/Navigation.js';
 import './App.css';
 
 // Import pages you have completed:
 // Home, Topics, Gallery, Contact, and Staff Pages 
 
 
-import HobbiesPage from './pages/HobbiesPage';
+import HobbiesPage from './pages/HobbiesPage.js';
 
 // For Create and Edit, use the form OR table design; not both.
 // If your schema requires LONG data input, then use the FORM design:
-import AddHobbyPageForm from './pages/AddHobbyPageForm';
-import EditHobbyPageForm from './pages/EditHobbyPageForm';
+import AddHobbyPageForm from './pages/AddHobbyPageForm.js';
+import EditHobbyPageForm from './pages/EditHobbyPageForm.js';
 
 
 
@@ -23,7 +23,7 @@ import EditHobbyPageForm from './pages/EditHobbyPageForm';
 import TopicsPage from './pages/TopicsPage.js';
 import HomePage from './pages/HomePage.js';
 import GalleryPage from './pages/GalleryPage.js';
-import OrderPage from './pages/OrderPage';
+import OrderPage from './pages/OrderPage.js';
 
 
 // Define the function that renders the content in Routes, using State.
